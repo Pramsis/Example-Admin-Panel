66f4fdfe67f386d968f1e9d07e6d89a7a6571d08
Dimas Pramudya <dimasrajawali76@gmail.com>
2017-11-26 13:53:25 +0700

    first

---
+++ index.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="stylesheet" type="text/css" href="css/style.css">
+<link rel="stylesheet" type="text/css" href="css/font-awesome.min.css">
+</head>
+<body>
+
+<ul class="sidenav">
+  <li><a class="active" href="#home">Beranda</a></li>
+  <li><a href="#news">Data Admin</a></li>
+  <li><a href="#contact">Data Anggota</a></li>
+  <li><a href="#about">Data Topik</a></li>
+  <li><a href="#about">Logout</a></li>
+</ul>
+
+<div class="wrapper">
+  <div class="header">
+    <h2>OI Rent Car Admin Panel</h2>
+  </div>
+  <div class="content">
+    <p>This example use media queries to transform the sidenav to a top navigation bar when the screen size is 1050px or less.</p>
+    <p>We have also added a media query for screens that are 400px or less, which will vertically stack and center the navigation links.</p>
+    <p>You will learn more about media queries and responsive web design later in our CSS Tutorial.</p>
+    <h2>Beranda</h2>
+    <a href="#" class="btn info">Tambah Data</a>
+    <table>
+      <tr>
+        <th>Firstname</th>
+        <th>Lastname</th>
+        <th>Savings</th>
+        <th>Lastname</th>
+        <th>Savings</th>
+        <th>Aksi</th>
+      </tr>
+      <tr>
+        <td>Peter</td>
+        <td>Griffin</td>
+        <td>Peter</td>
+        <td>Griffin</td>
+        <td>$100</td>
+        <td>
+          <a href="#" class="btn info"><i class="fa fa-pencil"></i></a>
+          &nbsp;
+          <a href="#" class="btn danger"><i class="fa fa-trash"></i></a>
+        </td>
+      </tr>
+      <tr>
+        <td>Peter</td>
+        <td>Griffin</td>
+        <td>Peter</td>
+        <td>Griffin</td>
+        <td>$100</td>
+        <td>
+          <a href="#" class="btn info"><i class="fa fa-pencil"></i></a>
+          &nbsp;
+          <a href="#" class="btn danger"><i class="fa fa-trash"></i></a>
+        </td>
+      </tr>
+      <tr>
+        <td>Peter</td>
+        <td>Griffin</td>
+        <td>Peter</td>
+        <td>Griffin</td>
+        <td>$100</td>
+        <td>
+          <a href="#" class="btn info"><i class="fa fa-pencil"></i></a>
+          &nbsp;
+          <a href="#" class="btn danger"><i class="fa fa-trash"></i></a>
+        </td>
+      </tr>
+      <tr>
+        <td>Peter</td>
+        <td>Griffin</td>
+        <td>Peter</td>
+        <td>Griffin</td>
+        <td>$100</td>
+        <td>
+          <a href="#" class="btn info"><i class="fa fa-pencil"></i></a>
+          &nbsp;
+          <a href="#" class="btn danger"><i class="fa fa-trash"></i></a>
+        </td>
+      </tr>
+    </table>
+
+  </div>
+  <div class="footer">
+    <p>&copy; Responsive Sidenav Admin Panel @ Dinda Nuraini</p>
+  </div>
+</div>
+
+</body>
+</html>
---
+++ login.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Login - Admin</title>
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <link rel="stylesheet" type="text/css" href="css/font-awesome.min.css">
+</head>
+<body>
+<div class="form-login">
+    <h1>Login</h1>
+  <form method="post">
+    <i class="fa fa-user"></i>&nbsp;<label for="fname">Username</label>
+    <input type="text" id="fname" name="firstname">
+    <i class="fa fa-lock"></i>&nbsp;<label for="lname">Password</label>
+    <input type="password" id="lname" name="lastname">
+    <input type="submit" value="Login">
+  </form>
+</div>
+</body>
+</html>
